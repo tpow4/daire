b79e7cbac91a6e9008f8a3aab072282a76bad957
Tom Powers <17350105+tpow4@users.noreply.github.com>
2023-01-20 00:31:05 -0600

    Add toolbar implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import * as React from 'react';
+import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme, Toolbar } from '@mui/material';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import createEmotionCache from '../utilities/createEmotionCache';
+import lightThemeOptions from '../styles/themes/lightThemeOptions';
+import '../styles/globals.css';
+import ResponsiveAppBar from './toolbar';
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+const lightTheme = createTheme(lightThemeOptions);
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <ResponsiveAppBar/>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
